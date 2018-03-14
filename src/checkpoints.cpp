ee6204c5893c1bbd2e4f292b9ff050acd603e2be
humdevs <36679024+humdevs@users.noreply.github.com>
2018-03-14 02:21:08 -0400

    final

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -1,4 +1,35 @@
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#include <boost/assign/list_of.hpp> // for 'map_list_of()'
+#include <boost/foreach.hpp>
+
+#include "checkpoints.h"
+
+#include "txdb.h"
+#include "main.h"
+#include "uint256.h"
+
+
+static const int nCheckpointSpan = 10;
+
+namespace Checkpoints
+{
+    typedef std::map<int, uint256> MapCheckpoints;
+
+    //
+    // What makes a good checkpoint block?
+    // + Is surrounded by blocks with reasonable timestamps
+    //   (no blocks before with a timestamp after, none after with
+    //    timestamp before)
+    // + Contains no strange transactions
+    //
+    static MapCheckpoints mapCheckpoints =
+        boost::assign::map_list_of
+        ( 0,      hashGenesisBlock )
+    ;
+    
     // TestNet has no checkpoints
     static MapCheckpoints mapCheckpointsTestnet =
         boost::assign::map_list_of
